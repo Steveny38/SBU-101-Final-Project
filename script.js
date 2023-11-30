38fda2eec55747d66e67350b16a3ad83dfefda7e
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2023-11-30 01:41:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const DOMSelectors = {
+    animation: document.getElementsByClassName("animation"),
+    links: document.getElementsByClassName("rlink"),
+    alinks: document.getElementsByClassName("alink"),
+}
+console.log(DOMSelectors);
+
+
+function changeClass(){
+    for(var i = 0; i < DOMSelectors.animation.length; i++){
+        var windowHeight = window.innerHeight;
+        var contentTop = DOMSelectors.animation[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+        console.log(contentTop < windowHeight-elementVisible)
+        if(contentTop < windowHeight-elementVisible){
+            DOMSelectors.animation[i].classList.add("active")
+        } else{
+            DOMSelectors.animation[i].classList.remove("active")
+        }
+    } 
+
+}
+
+function checkCurrentPage (){
+   
+    const currentURL = window.location.href;
+
+    for(var i = 0; i < DOMSelectors.alinks.length; i++){
+        
+        if(DOMSelectors.alinks[i].href == currentURL){
+            DOMSelectors.links[i].classList.add("active")
+        } else{
+            DOMSelectors.links[i].classList.remove("active")
+        }
+    
+}
+
+}
+
+window.addEventListener("scroll", changeClass )
+window.addEventListener("click" , checkCurrentPage )
+
+changeClass()
+checkCurrentPage()
